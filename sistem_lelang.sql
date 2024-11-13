baa17b9557bf041348b7f9e846f951d3295a3ba3
jidan <145263771+zaidan-bimantara@users.noreply.github.com>
2024-11-13 18:20:01 +0700

    Add files via upload

---
+++ sistem_lelang.sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Nov 2024 pada 12.18
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sistem_lelang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `history_lelang`
+--
+
+CREATE TABLE `history_lelang` (
+  `id_history` int(11) NOT NULL,
+  `id_lelang` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `penawaran_harga` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(25) NOT NULL,
+  `tgl` date NOT NULL,
+  `harga_awal` int(20) NOT NULL,
+  `deskripsi_barang` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`id_barang`, `nama_barang`, `tgl`, `harga_awal`, `deskripsi_barang`) VALUES
+(1, 'ultra milk', '2024-11-06', 5, 'susu asli pergunungan tinggi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_lelang`
+--
+
+CREATE TABLE `tb_lelang` (
+  `id_lelang` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `tgl_lelang` date NOT NULL,
+  `harga_akhir` int(20) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_petugas` int(11) NOT NULL,
+  `status` enum('dibuka','ditutup') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_lelang`
+--
+
+INSERT INTO `tb_lelang` (`id_lelang`, `id_barang`, `tgl_lelang`, `harga_akhir`, `id_user`, `id_petugas`, `status`) VALUES
+(1, 1, '2024-11-07', 0, 0, 1, 'dibuka');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_level`
+--
+
+CREATE TABLE `tb_level` (
+  `id_level` int(11) NOT NULL,
+  `level` enum('administrator','petugas') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_level`
+--
+
+INSERT INTO `tb_level` (`id_level`, `level`) VALUES
+(1, 'administrator'),
+(2, 'petugas');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_masyarakat`
+--
+
+CREATE TABLE `tb_masyarakat` (
+  `id_user` int(11) NOT NULL,
+  `nama_lengkap` varchar(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `telp` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_masyarakat`
+--
+
+INSERT INTO `tb_masyarakat` (`id_user`, `nama_lengkap`, `username`, `password`, `telp`) VALUES
+(1, 'ehek', 'ehekalanz', '12345', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_petugas`
+--
+
+CREATE TABLE `tb_petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `nama_petugas` varchar(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `id_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_petugas`
+--
+
+INSERT INTO `tb_petugas` (`id_petugas`, `nama_petugas`, `username`, `password`, `id_level`) VALUES
+(1, 'alan', 'alanzehek', '153917', 2),
+(2, 'ehek', 'ehekalanz', '153917', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `history_lelang`
+--
+ALTER TABLE `history_lelang`
+  ADD PRIMARY KEY (`id_history`);
+
+--
+-- Indeks untuk tabel `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indeks untuk tabel `tb_lelang`
+--
+ALTER TABLE `tb_lelang`
+  ADD PRIMARY KEY (`id_lelang`);
+
+--
+-- Indeks untuk tabel `tb_level`
+--
+ALTER TABLE `tb_level`
+  ADD PRIMARY KEY (`id_level`);
+
+--
+-- Indeks untuk tabel `tb_masyarakat`
+--
+ALTER TABLE `tb_masyarakat`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indeks untuk tabel `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `history_lelang`
+--
+ALTER TABLE `history_lelang`
+  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_lelang`
+--
+ALTER TABLE `tb_lelang`
+  MODIFY `id_lelang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_level`
+--
+ALTER TABLE `tb_level`
+  MODIFY `id_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_masyarakat`
+--
+ALTER TABLE `tb_masyarakat`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
